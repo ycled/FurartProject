7b1975002433f0ad1fcf8b41f198314ef07f47dc
YC <jessica.ece2012@gmail.com>
2013-03-21 19:51:34 -0400

    bbb

--- furart/forms.py
+++ furart/forms.py
@@ -1,19 +1,21 @@
 from django import forms
 
-#sample!! to be removed!!!
-class MessageForm(forms.Form): 
-    name = forms.CharField(required=True) 
-    email = forms.EmailField(required=True) 
-    homepage = forms.URLField(required=False) 
-    title = forms.CharField(required=True) 
-    content = forms.CharField(required=True)
-    
+
+ACTIVIRY_TYPE_CHOICES =  (('A', 'a'),('B', 'b'))
+
 
 #post new activity 
-class ActivityForm(forms.Form): 
-    title = forms.CharField(required=True) 
-    activitytype = forms.CharField(required=True) 
+class ActivityForm(forms.Form):
+    activitytype = forms.ChoiceField(widget=forms.Select, choices = ACTIVIRY_TYPE_CHOICES,label='Type') 
+    title = forms.CharField(required=True)     
+    time = forms.CharField(required=True) 
     organizor = forms.CharField(required=True) 
     location = forms.CharField(required=True) 
     detail = forms.CharField(required=True) 
-    time = forms.CharField(required=True) 
+    #poster, current post user, not equal to organizor
+    #time: start- end, choice widget
+    
+    
+# # search activity by type
+# class ActivitySearchForm(forms.Form):
+#     activitytype = forms.ChoiceField(widget=forms.Select, choices = ACTIVIRY_TYPE_CHOICES,label='Type') 
