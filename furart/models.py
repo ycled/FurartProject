10958296ba1d156ccab82b34461672b5b55ce9cd
YC <jessica.ece2012@gmail.com>
2013-03-20 20:45:28 -0400

    basic post and search

--- furart/models.py
+++ furart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    homepage = models.URLField()
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=500)
+    time = models.DateTimeField(auto_now_add=True)
+    ip = models.CharField(max_length=20)
+    
+    
+class Activity(models.Model):
+    title = models.CharField(max_length=200)
+    activitytype = models.CharField(max_length=30)
+    organizor = models.CharField(max_length=30)
+    location = models.CharField(max_length=200)
+    detail = models.CharField(max_length=500)
+    time = models.DateTimeField(auto_now_add=True)
--- furart/urls.py
+++ furart/urls.py
@@ -4,5 +4,12 @@ from furart import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'signup.html', views.signup, name='signup')
+    url(r'signup.html', views.signup, name='signup'),
+    
+    url(r'^activity/$', views.activity),
+    url(r'^message/$', views.message),
+    url(r'^activity_post/$', views.activity_post),
+    url(r'^activity_post_success/$', views.activity_post_success),
+    url(r'^activity_search/$', views.activity_search),
+    url(r'^activity_search_result/$', views.activity_search_result), 
 )
